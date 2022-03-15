d45723df39153e2365f03422b1d73c921a1292ce
golosalex <Golosalex@mail.ru>
2022-03-15 10:41:31 +0300

    Add project files.

---
+++ RelativeSourceEx/RangeConverter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace RelativeSourceEx
+{
+    class RangeConverter:IValueConverter
+    {
+        
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            bool result = false;
+            ProcessStage stage = (ProcessStage)value;
+            int param;
+            Int32.TryParse(parameter as string, out param);
+            int intStage = ProcessStageToInt(stage);
+            return intStage >= param;
+        }
+        private static int ProcessStageToInt(ProcessStage ps)
+        {
+            int pv = 1;
+            switch (ps)
+            {
+                case ProcessStage.Stage1:
+                    pv = 1;
+                    break;
+                case ProcessStage.Stage2:
+                    pv = 2;
+                    break;
+                case ProcessStage.Stage3:
+                    pv =3;
+                    break;
+                case ProcessStage.Stage4:
+                    pv = 4;
+                    break;
+                case ProcessStage.Stage5:
+                    pv = 5;
+                    break;
+            }
+            return pv;
+        }
+        
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RelativeSourceEx/StageHelper.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace RelativeSourceEx
+{
+	[Flags]
+	public enum ProcessStage
+	{
+		Stage1 = 0,
+		Stage2 = 2,
+		Stage3 = 4,
+		Stage4 = 8,
+		Stage5 = 16
+	}
+	public class StageHelper : DependencyObject
+    {
+		public static readonly DependencyProperty ProcessCompletionProperty = DependencyProperty.RegisterAttached(
+		"ProcessCompletion", typeof(double), typeof(StageHelper), new PropertyMetadata(0.0, OnProcessCompletionChanged));
+
+		private static readonly DependencyPropertyKey ProcessStagePropertyKey = DependencyProperty.RegisterAttachedReadOnly(
+		"ProcessStage", typeof(ProcessStage), typeof(StageHelper), new PropertyMetadata(ProcessStage.Stage1));
+
+		public static readonly DependencyProperty ProcessStageProperty = ProcessStagePropertyKey.DependencyProperty;
+
+		private static void OnProcessCompletionChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+		{
+			double progress = (double)e.NewValue;
+			ProgressBar bar = d as ProgressBar;
+			if (progress >= 0 && progress < 20) bar.SetValue(ProcessStagePropertyKey, ProcessStage.Stage1);
+			if (progress >= 20 && progress < 40) bar.SetValue(ProcessStagePropertyKey, ProcessStage.Stage2 | ProcessStage.Stage2);
+			if (progress >= 40 && progress < 60) bar.SetValue(ProcessStagePropertyKey, ProcessStage.Stage3);
+			if (progress >= 60 && progress < 80) bar.SetValue(ProcessStagePropertyKey, ProcessStage.Stage4);
+			if (progress >= 80 && progress <= 100) bar.SetValue(ProcessStagePropertyKey, ProcessStage.Stage5);
+		}
+		//[AttachedPropertyBrowsableForType(typeof(Control))]
+		//public static ProcessStage GetProcessStage(ProgressBar bar)
+  //      {
+		//	if (bar == null) throw new ArgumentException("fignia");
+		//	return (ProcessStage)bar.GetValue(ProcessStageProperty);
+  //      }
+		public static void SetProcessCompletion(ProgressBar bar, double progress)
+		{
+			bar.SetValue(ProcessCompletionProperty, progress);
+		}
+
+		public static void SetProcessStage(ProgressBar bar, ProcessStage stage)
+		{
+			bar.SetValue(ProcessStagePropertyKey, stage);
+		}
+	}
+}
